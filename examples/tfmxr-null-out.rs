f33bfcc67c38c32c2952fb5c440e5b3ec780d9b4
crumblingstatue <radiantstatue@gmail.com>
2024-11-05 22:08:48 +0100

    Make tfmxr-null-out more useful for panic testing

--- examples/tfmxr-null-out.rs
+++ examples/tfmxr-null-out.rs
@@ -1,23 +1,36 @@
 //! For miri testing
 
-use {std::ops::ControlFlow, tfmxr::PlayerBuilder};
+use {
+    std::ops::ControlFlow,
+    tfmxr::{PlayerBuilder, PlayerCmd},
+};
 
 fn main() {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .parse_env("RUST_LOG")
+        .init();
     let mut player = PlayerBuilder::new(std::env::args().nth(1).expect("Need mdat file"))
         .build()
         .unwrap();
     let begin = std::time::Instant::now();
     eprintln!("Let's-a-go!");
-    let mut total = 0;
+    let mut song_total = 0;
     player.play(|samples| {
-        total += samples.len();
+        song_total += samples.len();
+        let realtime = begin.elapsed().as_secs_f32();
+        // 44khz, 2 channels, s16le
+        let render_elapsed = song_total / (44_100 * 2 * 2);
         eprint!(
-            "[tfmxr] ({:.02}) {} bytes rendered (approx {} seconds)\r",
-            begin.elapsed().as_secs_f32(),
-            total,
-            // 44khz, 2 channels, s16le
-            total / (44_100 * 2 * 2)
+            "[tfmxr] ({realtime:.02}) {song_total} bytes rendered (approx {render_elapsed} seconds)\r",
         );
-        ControlFlow::Break(())
+        // After 2 hours of rendered song, it's probably enough testing
+        if render_elapsed > 7200 {
+            eprintln!("Enough. Requesting next song...");
+            song_total = 0;
+            ControlFlow::Continue(Some(PlayerCmd::Next))
+        } else {
+            ControlFlow::Continue(None)
+        }
     });
 }
